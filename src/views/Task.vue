bbe9eb0cc261613a06f43d79fafca216e892d965
unknown <helio@solucx.com.br>
2021-04-15 13:52:58 -0300

    Fix native link

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -4,16 +4,20 @@
       <input
         type="text"
         :value="task.name"
-        class="p-2 w-full mr-2 flex-grow text-xl font-bold"
+        class="p-2 w-full mr-2 mb-2 flex-grow text-xl font-bold"
         @change="updateTaskProperty($event, 'name')"
         @keyup.enter="updateTaskProperty($event, 'name')"
       />
 
-      <textarea
-        class="relative w-full bg-transparent px-2 border mt-2 h-64 border-none leading-normal"
-        :value="task.description"
-        @change="updateTaskProperty($event, 'description')"
-      ></textarea>
+      <div class="px-2 w-full">
+        <label>Description:</label>
+        <textarea
+          class="relative w-full bg-white p-2 border mt-2 h-64 leading-normal"
+          placeholder="Insert your task description"
+          :value="task.description"
+          @change="updateTaskProperty($event, 'description')"
+        ></textarea>
+      </div>
     </div>
   </div>
 </template>
